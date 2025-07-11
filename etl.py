d0d68f451e7f22d6c49b77b6484b9911fb9d7eff
Karla Suarez <ksuarez@bio.com>
2025-07-10 22:17:55 -0500

    Commit tercer cambio

--- etl.py
+++ etl.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 df = pd.read_csv('input.csv')
-df['processed'] = df['value'] * 10
+df['processed'] = df['value'] * 100
 df['processed_inv'] = df['processed'] / 10
 df.to_csv('output1.csv', index=False)
 print("ETL completado.")
